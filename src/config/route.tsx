eaced104a692f555c85b2cd832bac5f36aa70c02
Ones <964981348@qq.com>
2020-01-15 17:58:22 +0800

    完成权限管理

--- src/config/route.tsx
+++ src/config/route.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {
   Route,
+  Redirect
 } from "react-router-dom";
 
 import NoAauth from '../pages/Authorized/404'
 import NotFonud from '../pages/Authorized/403'
 import BasicLayout from '../pages/layout/BasicLayout'
+import { subsetTo } from '../utils/utils'
 
+// 由于官方不支持路由嵌套,因此router-config没有子路由的概念,都是平级的,如果想实现子路由,可以递归,然后动态生成打平了的<Router>
 const routes = [
   {
     path: "/login",
@@ -15,22 +18,31 @@ const routes = [
   {
     path: "/123",
     component: BasicLayout,
-    exact:true
+    exact: true,
+    auth: [1, 2]
   },
   {
     path: "/123/404",
     component: NotFonud
+  },
+  {
+    path: "/403",
+    component: NoAauth
   }
 ];
 
 export function SubRoutes(route: any) {
+  const auth: string[] | undefined = localStorage.getItem("auth")?.split(",");
+  const hasAuth = subsetTo(route.auth, auth);
   return (
-    <Route
-      path={route.path}
-      render={props => (
-        <route.component {...props} routes={route.routes} />
-      )}
-    />
+    hasAuth ?
+      <Route
+        path={route.path}
+        render={props => (
+          <route.component {...props} routes={route.routes} />
+        )}
+      /> :
+      <Redirect to={{ pathname: "/403" }} />
   );
 }
 
